9785101adbda8c0b6a481a3343ef0ba204c65c64
Harsh Vardhan Singh <hvsinghrathore28@gmail.com>
2026-02-09 15:55:51 +0530

    feat: add show more / show less toggle for certificates and projects

--- src/components/DisplayCertificate.jsx
+++ src/components/DisplayCertificate.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import CertificateCard from './CertificateCard'
 import { certificates } from '../data/certificates'
+import up from '../assets/up.png'
+import down from '../assets/down.png'
 const DisplayCertificate = () => {
+  const [show, setshow] = useState(false);
+  const size = certificates.length;
+  const showCertificates = () => {
+    show?setshow(false):setshow(true)
+  };
+
+
+
   return (
-    <div className='grid gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-8'>
-        {
-            certificates.map((certificate)=>(
-                <CertificateCard image={certificate.image} cname={certificate.cname} issuedby={certificate.issuedby} date={certificate.date} Verify={certificate.Verify} />
-            ))
-        }
+    <div >
+      <div className='grid gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-8'>
+          {
+            (show ? certificates : certificates.filter(c => c.id <= 3) )
+              .map(certificate => (
+                <CertificateCard
+                  key={certificate.id}
+                  image={certificate.image}
+                  cname={certificate.cname}
+                  issuedby={certificate.issuedby}
+                  date={certificate.date}
+                  Verify={certificate.Verify}
+                />
+              ))
+}
+      </div>
+      <div className={'flex justify-end mt-5 ${size>=3:"block":"hidden"}'}>
+        <div className='text-white px-2 py-1 lg:text-xl font-bold lg:px-3 flex gap-2 justify-center items-center hover:scale-[1.05] hover:bg-white/20 transition-all duration-300 bg-white/20 rounded'>
+          <button  onClick={showCertificates}>{show?"Show less":"Show more"}</button>
+          {show?<img className='w-3 h-4' src={up}/>:<img className='w-3 h-4' src={down}/>}
+        </div>
+      </div>
+        
     </div>
+
   )
 }
 
--- src/components/DisplayProjects.jsx
+++ src/components/DisplayProjects.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { projects } from '../data/projects'
 import ProjectCard from './ProjectCard'
+import up from '../assets/up.png'
+import down from '../assets/down.png'
 const DisplayProjects = () => {
+  const [show, setshow] = useState(false);
+  const size = projects.length;
+  const showProjects = () => {
+    show?setshow(false):setshow(true)
+  };
+
+
+
   return (
-    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-8">
-            {projects.map((project)=>(
+    <div >
+      <div className='grid gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-8'>
+          {
+            (show ? projects : projects.filter(c => c.id <= 3) )
+              .map((project)=>(
                 <ProjectCard key={project.id} image={project.image} techStack={project.techStack} name={project.name} description={project.description}  github={project.github}/>
-            ))}
+              ))
+}
+      </div>
+      <div className={`flex justify-end mt-5 ${size >3?"block":"hidden"}`}>
+        <div className='text-white px-2 py-1 lg:text-xl font-bold lg:px-3 flex gap-2 justify-center items-center hover:scale-[1.05] hover:bg-white/20 transition-all duration-300 bg-white/20 rounded'>
+          <button  onClick={showProjects}>{show?"Show less":"Show more"}</button>
+          {show?<img className='w-3 h-4' src={up}/>:<img className='w-3 h-4' src={down}/>}
+        </div>
+      </div>
+        
     </div>
+
   )
 }
+//   {
+//   return (
+//     <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-8">
+//             {projects.map((project)=>(
+//                 <ProjectCard key={project.id} image={project.image} techStack={project.techStack} name={project.name} description={project.description}  github={project.github}/>
+//             ))}
+//     </div>
+//   )
+// }
 
 export default DisplayProjects
